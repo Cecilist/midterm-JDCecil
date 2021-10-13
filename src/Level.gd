a9caa6229aca91c518464879412bbbf6f1bbe36a
Jacob Cecil <jdcecil@bsu.edu>
2021-10-13 00:29:21 -0400

    Added platforms that can be jumped through. Added a placeholder game over screen. Added a box of death that moves up and triggers game over screen. Remade test level

--- src/Level.gd
+++ src/Level.gd
@@ -9,3 +9,11 @@ func _spawn_Player():
 		_player = load("res://src/Player.tscn").instance()
 		_player.position = $SpawnPoint.position
 		add_child(_player)
+
+
+func _on_Lava_body_entered(body):
+	if body == _player:
+		_game_Over()
+		
+func _game_Over():
+	var _ignored := get_tree().change_scene("res://src/GameOver.tscn")
