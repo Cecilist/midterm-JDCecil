766375c466976130e0a8fb61cea9afe948538dcb
Jacob Cecil <jdcecil@bsu.edu>
2021-10-14 00:16:45 -0400

    Added coin counter and a single coin. Redid test level.

--- src/Lava.gd
+++ src/Lava.gd
@@ -1,9 +1,5 @@
 extends Area2D
 
-var direction := Vector2.ZERO
-
-onready var _tween = get_node("Tween")
-
 func _physics_process(delta):
 	move_local_y(-100*delta)
 
--- src/Level.gd
+++ src/Level.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+
+var coinsCollected : int = 5 
 var _player : KinematicBody2D
+
+
 
 func _ready():
 	_spawn_Player()
@@ -17,3 +21,10 @@ func _on_Lava_body_entered(body):
 		
 func _game_Over():
 	var _ignored := get_tree().change_scene("res://src/GameOver.tscn")
+
+
+func _on_Coin_body_entered(body):
+	if body == _player:
+		coinsCollected += 1
+		$HUD/CoinsLabel.text = "Coins Collected: " + str(coinsCollected) + "/ 10"
+		$Coin.queue_free()
