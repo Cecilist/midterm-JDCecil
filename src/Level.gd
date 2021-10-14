eb47fbb75cc4a8f76e7c8e0f879c2b58c5b73fd5
Jacob Cecil <jdcecil@bsu.edu>
2021-10-14 09:35:10 -0400

    Added goal area, and appropriate win screens

--- src/Level.gd
+++ src/Level.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-
-var coinsCollected : int = 5 
+var totalCoins : int = 10
+# Since we start "half-way there" we start with half the coins already collected
+var coinsCollected : int = totalCoins / 2
 var _player : KinematicBody2D
-
 
 
 func _ready():
@@ -28,3 +28,10 @@ func _on_Coin_body_entered(body):
 		coinsCollected += 1
 		$HUD/CoinsLabel.text = "Coins Collected: " + str(coinsCollected) + " / 10"
 		
+
+
+func _on_Goal_body_entered(body):
+	if body == _player and coinsCollected  >= totalCoins:
+		var _ignored := get_tree().change_scene("res://src/AllCoinsWin.tscn")
+	elif body == _player and coinsCollected  < totalCoins:
+		var _ignored := get_tree().change_scene("res://src/NotAllCoinsWin.tscn")
